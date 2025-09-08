8fc3de91cb3f412a140ac46686fc6316dbd389a5
Dhanya Gautam <dhanyag3001@gmail.com>
2025-09-09 01:56:10 +0530

    Create something-weird.cpp
    sir ka kachra

---
+++ something-weird.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class base{
+    int i;
+    public:
+    int j,k;
+    void seti(int x){
+        i=x;
+    }
+    int geti(){
+        return i;
+    }
+};
+class derived :public base{
+    public:
+    int a;
+    using base::j;
+    using base::seti;
+    using base::geti;
+    // using base::i;
+    
+};
+int main(){
+    derived ob;
+    ob.j=20;
+    ob.a=40;
+    ob.seti(10);
+    //ob.i=30;
+}
