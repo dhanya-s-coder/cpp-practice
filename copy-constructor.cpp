348eb8bf1e27de16468e822702d805e98263713e
Dhanya Gautam <dhanyag3001@gmail.com>
2025-09-10 16:49:01 +0530

    double-deletion

---
+++ copy-constructor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class resource{
+    int *data;
+    public:
+    resource(int value){
+        data=new int(value);
+        
+    }
+    int getValue(){
+        return *data;
+        
+    }
+    void setValue(int value){
+        *data=value;
+    }
+    ~resource(){
+        delete data;
+    }
+};
+int main(){
+    resource res1(10);
+    resource res2=res1;
+    res1.setValue(20);
+    cout<<res1.getValue();
+}
+
